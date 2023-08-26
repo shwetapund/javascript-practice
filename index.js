9b941d9af91ba52123df30a6393c60eda768ebda
shwetapund <pundshweta2020@gmail.com>
2023-08-25 20:39:47 -0700

    basic practice added

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hey i am shweta");
+
+//let
+let age = 20;
+console.log('age: '+age);
+
+let name = "shweta";
+console.log(name);
+name = "sai";
+console.log(name);
+
+//const 
+
+const Age = 20;
+console.log(Age);
+
+//var 
+var Name = "shweta";
+console.log(Name);
+
